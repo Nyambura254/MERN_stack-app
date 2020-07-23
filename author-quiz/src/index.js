6f071739c1a4b93d45a30d4e3f1829db5efae5c0
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-23 18:05:10 +0300

    author quiz game in creat-react-app

--- author-quiz/src/index.js
+++ author-quiz/src/index.js
@@ -8,12 +8,82 @@ import * as serviceWorker from './serviceWorker';
 const authors = [
   {
     name: "Mark Twain",
-    imageUrl: "../images/authors/marktwain.jpeg",
-    imageSource: "Wikipedia Commos",
-    books: ["The Adventures of Huckleberry Finn"]
+    imageUrl: "images/authors/marktwain.jpeg",
+    imageSource: "Wikimedia Commos",
+    books: ["The Adventures of Huckleberry Finn",
+      "The Christimas Carol",
+      "The Shinning"
+    ]
+
+
+  },
+  {
+    name: "William Shakespear",
+    imageUrl: "images/authors/marktwain.jpeg",
+    imageSource: "Wikimedia Commos",
+    books: ["Sonnet 116",
+      "The expression",
+      "The cuckoo's Calling"
+    ]
+
+
+  },
+  {
+    name: "Stephen King",
+    imageUrl: "images/authors/marktwain.jpeg",
+    imageSource: "Wikimedia Commos",
+    books: ["It",
+      "The rape of lucrece",
+      "Hard times"
+    ]
+
+
+  },
+  {
+    name: "Joseph Conrad",
+    imageUrl: "images/authors/marktwain.jpeg",
+    imageSource: "Wikimedia Commos",
+    books: ["Chance",
+      "The outsider",
+      "Misery"
+    ]
+
+
+  },
+  {
+    name: "JK Rowling",
+    imageUrl: "images/authors/marktwain.jpeg",
+    imageSource: "Wikimedia Commos",
+    books: ["Harry Potter",
+      "The innocents abroad",
+      "Four tragedies"
+    ]
+
+
+  },
+  {
+    name: "Charles Dickens",
+    imageUrl: "images/authors/marktwain.jpeg",
+    imageSource: "Wikimedia Commos",
+    books: ["The Adventures of Huckleberry Finn",
+      "The Christimas Carol",
+      "The Oxford"
+    ]
+
+
+  },
+  {
+    name: "Charles Darwin",
+    imageUrl: "images/authors/charlesdarwin.jpeg",
+    imageSource: "Wikimedia Commos",
+    books: ["On the origin of spicies",
+      "Hard Times",
+      "The stand"
+    ]
 
 
   }
+
 ];
 const state = {
   turnData: {
