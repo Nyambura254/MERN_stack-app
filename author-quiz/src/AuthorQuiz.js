0da20400e218d680e4c222295f4832563be138f8
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-23 14:26:32 +0300

    author quiz game in creat-react-app

---
+++ author-quiz/src/AuthorQuiz.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+import { render } from 'react-dom';
+
+function Hero() {
+  return (<div className="jumbotron col-10 offset-1">
+    <h1>Author Quiz Game</h1>
+    <p>Select the book written by the author shown</p>
+  </div>)
+}
+function Turn() {
+  return (
+    <div></div>
+  )
+}
+function Continue() {
+  return (<div></div>)
+}
+function Footer() {
+  return (
+    <div id="footer" className="row">
+      <div className="col-12">
+        <p className="text-muted credit">Find images from
+        <a href="https://commons.wikimedia.org/wiki/Main_Page" /></p>
+      </div>
+    </div>
+  )
+}
+
+class AuthorQuiz extends Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <Hero />
+        <Turn />
+        <Continue />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+
+export default AuthorQuiz;
